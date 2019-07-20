6e26c7ad300e52f2177434b22d43d0510ce230e0
TannorWallace <tannor.wallace@gmail.com>
2019-07-20 00:54:23 -0600

    halfway through comments

--- client/src/views/Rant.vue
+++ client/src/views/Rant.vue
@@ -4,25 +4,28 @@
     <h4>{{rant.title}}</h4>
     <p>{{rant.author}}</p>
     <p>{{rant.description}}</p>
-
     <button class="btn btn-outline-danger " @click="deleteRant">Delete Rant</button><br>
-    <form action="">
+
+    <form @submit.prevent="addComment">
       <div class="input-group mb-3 mt-3">
         <div class="input-group-prepend">
           <span class="input-group-text" id="basic-addon1">Author</span>
         </div>
         <input type="text" class="form-control text-center" placeholder="Author..." aria-label="Username"
-          aria-describedby="basic-addon1">
+          aria-describedby="basic-addon1" v-model="newComment.author">
       </div>
       <div class="input-group">
         <div class="input-group-prepend">
           <span class="input-group-text">Comment here</span>
         </div>
-        <textarea class="form-control" aria-label="With textarea"></textarea>
+        <textarea class="form-control" aria-label="With textarea" v-model="newComment.content"></textarea>
       </div>
-      <button class=" mt-1 btn btn-outline-warning border border-dark text-body" @click="addComment">Submit
+      <button class=" mt-1 btn btn-outline-warning border border-dark text-body" type="submit">Submit
         Comment</button>
     </form>
+    <div class="comment" v-for="comment in rant">
+      <h3>{{comment.author}}</h3>
+    </div>
 
   </div>
 </template>
@@ -30,9 +33,14 @@
 
 <script>
   export default {
+    // name: ['rant', 'comment'],
     name: 'rant',
     data() {
-      return {}
+      return {
+        newComment: {
+          rant: this.$route.params.rantId
+        }
+      }
     },
     mounted() {
       let dataToSend = {
@@ -43,14 +51,18 @@
     computed: {
       rant() {
         return this.$store.state.activeRant
+
       }
     },
     methods: {
       deleteRant() {
         this.$store.dispatch('deleteRantById', this.rant._id)
       },
+      viewComment(comment) {
+        this.$router.push({ name: 'comment', params: { commentId: comment._id } })
+      },
       addComment() {
-        this.$store.dispatch('addComment', this.rant._id)
+        this.$store.dispatch('addComment', this.newComment)
       }
     },
 
