53be85a0e5e6211104dd19a475226b045297b84a
TannorWallace <tannor.wallace@gmail.com>
2019-07-20 02:22:31 -0600

    Adds anon default author

--- server/services/CommentsService.js
+++ server/services/CommentsService.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 let ObjectId = mongoose.Schema.Types.ObjectId
 
 let _schema = new mongoose.Schema({
-  author: { type: String, required: true },
+  author: { type: ObjectId, ref: 'Author', required: true, default: '5d32af22f78b3e0880356b5a' }, // default to anon
   content: { type: String, required: true },
   votes: [{ type: String/* './models/Vote' */ }],
   rant: { type: ObjectId, ref: 'Rant', required: true }
--- server/services/RantService.js
+++ server/services/RantService.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose'
 let ObjectId = mongoose.Schema.Types.ObjectId
 
 let _schema = new mongoose.Schema({
-    title: { type: String, maxlength: 60, required: true },
-    // slug: { type: String, lowercase: true, unique: true }, 
-    author: { type: String, required: true },
-    description: { type: String, required: true },
-    image: [{ type: String }],
-    topic: { type: ObjectId, ref: 'Topic', required: true },
-    votes: [{ type: String/* './models/Vote' */ }]
+  title: { type: String, maxlength: 60, required: true },
+  // slug: { type: String, lowercase: true, unique: true }, 
+  author: { type: ObjectId, ref: 'Author', required: true, default: '5d32af22f78b3e0880356b5a' }, // default to anon
+  description: { type: String, required: true },
+  image: [{ type: String }],
+  topic: { type: ObjectId, ref: 'Topic', required: true },
+  votes: [{ type: String/* './models/Vote' */ }]
 }, { timestamps: true })
 
 export default mongoose.model('Rant', _schema)
